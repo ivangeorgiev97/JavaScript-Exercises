7a1034dd618395c39056f2be7901cf987e041c59
ivangeorgiev97 <georgiev1313@abv.bg>
2019-03-24 19:39:38 +0200

    Add exercise with destructuring

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const myFavouriteNumbers = [3, 6, 13, 49];
+[favNum0, favNum1] = myFavouriteNumbers;
+[, , favNum2, favNum3] = myFavouriteNumbers;
+
+console.log("myFavouriteNumbers:");
+console.log(myFavouriteNumbers);
+
+console.log("first and second number from myFavouriteNumbers:");
+console.log(favNum0, favNum1);
+
+console.log("third and fourth number from myFavouriteNumbers:");
+console.log(favNum2, favNum3);
